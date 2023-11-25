28a53dd29cc756e6f8f09c3324e247ef65e83960
Aaron Chan <aarchan108@gmail.com>
2023-11-25 03:07:44 -0500

    SHT30D can get data if error is ignroed. tmp117 and lps seem to get stuck fetching values

--- app/src/sensor_svc.c
+++ app/src/sensor_svc.c
@@ -15,12 +15,13 @@ int check_dev(const struct device *device) {
 
 static void update_sht30d_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ONE(sensirion_sht3xd);
-    if (check_dev(device)) return;
 
     struct sensor_value temperature;   
     struct sensor_value humidity;
 
-
+    int ret = device_is_ready(device);
+    if (ret) printk("Error %d: %s is not ready", ret, device->name);
+    
     while (1) {
         if (sensor_sample_fetch(device)) continue;
 
@@ -37,14 +38,17 @@ static void update_sht30d_readings(void *unused0, void *unused1, void *unused2) 
 
 static void update_lps22hb_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ANY(st_lps22hv);
-    if (check_dev(device)) return;
 
     struct sensor_value temperature;   
     struct sensor_value pressure;
 
+    int ret = device_is_ready(device);
+    if (ret) printk("Error %d: %s is not ready", ret, device->name);
 
     while (1) {
-        if (sensor_sample_fetch(device)) continue;
+        if (sensor_sample_fetch(device)) {
+            printk("LPS22HB: Failed to get data");
+        }
 
         sensor_channel_get(device, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
         sensor_channel_get(device, SENSOR_CHAN_PRESS, &pressure);
@@ -52,7 +56,7 @@ static void update_lps22hb_readings(void *unused0, void *unused1, void *unused2)
         readings.lps22hb_temp = sensor_value_to_float(&temperature);
         readings.lps22hb_press = sensor_value_to_float(&pressure);
 
-        printk("SHT30D: %f C\t %f Pa\n", readings.lps22hb_temp, readings.lps22hb_press);
+        printk("LPS22HB: %f C\t %f Pa\n", readings.lps22hb_temp, readings.lps22hb_press);
 
         k_msleep(100);
     }
@@ -62,10 +66,11 @@ static void update_lps22hb_readings(void *unused0, void *unused1, void *unused2)
 
 static void update_tmp117_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ANY(ti_tmp116);
-    if (check_dev(device)) return;
 
     struct sensor_value temperature;   
 
+    int ret = device_is_ready(device);
+    if (ret) printk("Error %d: %s is not ready", ret, device->name);
 
     while (1) {
         if (sensor_sample_fetch(device)) continue;
@@ -83,8 +88,8 @@ static void update_tmp117_readings(void *unused0, void *unused1, void *unused2) 
 
 
 int sensor_init() {
-    update_sht30d_readings(NULL, NULL, NULL);
-    update_tmp117_readings(NULL, NULL, NULL);
+    // update_sht30d_readings(NULL, NULL, NULL);
+    // update_tmp117_readings(NULL, NULL, NULL);
     update_lps22hb_readings(NULL, NULL, NULL);
     
     return 0;
