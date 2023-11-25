e229362c5d70700d3c775387c3abe7d334d64742
Aaron Chan <aarchan108@gmail.com>
2023-11-25 03:29:20 -0500

    fetch stops all threads from being able to fetch. sample get just blocks the thread from continuing

--- app/src/sensor_svc.c
+++ app/src/sensor_svc.c
@@ -27,23 +27,22 @@ int check_dev(const struct device *device) {
 static void update_sht30d_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ONE(sensirion_sht3xd);
 
-    struct sensor_value temperature;   
-    struct sensor_value humidity;
+    struct sensor_value temperature = {0};   
+    struct sensor_value humidity = {0};
 
     int ret = device_is_ready(device);
     if (ret) printk("Error %d: %s is not ready\n", ret, device->name);
     
     while (1) {
-        printk("SHT30D task executing\n");
-        // if (sensor_sample_fetch(device)) continue;
-        //
-        // sensor_channel_get(device, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
-        // sensor_channel_get(device, SENSOR_CHAN_HUMIDITY, &humidity);
-        //
-        // readings.sht30d_temp = sensor_value_to_float(&temperature);
-        // readings.sht30d_hum  = sensor_value_to_float(&humidity);
-        //
-        // printk("SHT30D: %f C\t %f Pa\n", readings.sht30d_temp, readings.sht30d_hum);
+        if (sensor_sample_fetch(device)) continue;
+        
+        sensor_channel_get(device, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+        sensor_channel_get(device, SENSOR_CHAN_HUMIDITY, &humidity);
+
+        readings.sht30d_temp = sensor_value_to_float(&temperature);
+        readings.sht30d_hum  = sensor_value_to_float(&humidity);
+
+        printk("SHT30D: %f C\t %f Pa\n", readings.sht30d_temp, readings.sht30d_hum);
         k_msleep(100);
     }
 }
@@ -51,26 +50,25 @@ static void update_sht30d_readings(void *unused0, void *unused1, void *unused2) 
 static void update_lps22hb_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ANY(st_lps22hv);
 
-    struct sensor_value temperature;   
-    struct sensor_value pressure;
+    struct sensor_value temperature = {0};   
+    struct sensor_value pressure = {0};
 
     int ret = device_is_ready(device);
     if (ret) printk("Error %d: %s is not ready\n", ret, device->name);
 
     while (1) {
-        printk("LPS22HB task executing\n");
         // if (sensor_sample_fetch(device)) {
         //     printk("LPS22HB: Failed to get data");
         // }
-        //
+
         // sensor_channel_get(device, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
         // sensor_channel_get(device, SENSOR_CHAN_PRESS, &pressure);
-        //
-        // readings.lps22hb_temp = sensor_value_to_float(&temperature);
-        // readings.lps22hb_press = sensor_value_to_float(&pressure);
-        //
-        // printk("LPS22HB: %f C\t %f Pa\n", readings.lps22hb_temp, readings.lps22hb_press);
-        //
+
+        readings.lps22hb_temp = sensor_value_to_float(&temperature);
+        readings.lps22hb_press = sensor_value_to_float(&pressure);
+
+        printk("LPS22HB: %f C\t %f Pa\n", readings.lps22hb_temp, readings.lps22hb_press);
+
         k_msleep(100);
     }
 
@@ -80,20 +78,19 @@ static void update_lps22hb_readings(void *unused0, void *unused1, void *unused2)
 static void update_tmp117_readings(void *unused0, void *unused1, void *unused2) {
     const struct device *device = DEVICE_DT_GET_ANY(ti_tmp116);
 
-    struct sensor_value temperature;   
+    struct sensor_value temperature = {0};   
 
     int ret = device_is_ready(device);
     if (ret) printk("Error %d: %s is not ready\n", ret, device->name);
 
     while (1) {
-        printk("TMP117 task executing\n");
         // if (sensor_sample_fetch(device)) continue;
-        //
+        
         // sensor_channel_get(device, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
-        //
-        // readings.tmp117_temp = sensor_value_to_float(&temperature);
-        //
-        // printk("TMP117: %f C\n", readings.tmp117_temp);
+
+        readings.tmp117_temp = sensor_value_to_float(&temperature);
+
+        printk("TMP117: %f C\n", readings.tmp117_temp);
         k_msleep(100);
     }
 
